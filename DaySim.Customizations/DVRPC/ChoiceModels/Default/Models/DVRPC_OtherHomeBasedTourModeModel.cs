316db14752e6e8fff8d502e167d4b2c26911a4cc
Tsay <will@wololo.co>
2025-10-01 12:30:46 -0400

    Update DVRPC_OtherHomeBasedTourModeModel.cs
    Make Other Home Based Tours - Transit consistent with Work Tour - PnR

--- DaySim.Customizations/DVRPC/ChoiceModels/Default/Models/DVRPC_OtherHomeBasedTourModeModel.cs
+++ DaySim.Customizations/DVRPC/ChoiceModels/Default/Models/DVRPC_OtherHomeBasedTourModeModel.cs
@@ -17,6 +17,9 @@ namespace DaySim.ChoiceModels.Default.Models {
       int coreCBDOrig = ((tour.OriginParcel.District == 1 || tour.OriginParcel.District == 2) && (tour.OriginParcel.ZoneKey < 200 || (tour.OriginParcel.ZoneKey > 1000 && tour.OriginParcel.ZoneKey < 1200))).ToFlag();// (tour.OriginParcel.HouseholdsBuffer1 + tour.OriginParcel.EmploymentTotalBuffer1 >= 31000).ToFlag();
       int coreCBDDest = ((destinationParcel.District == 1 || destinationParcel.District == 2) && (destinationParcel.ZoneKey < 200 || (destinationParcel.ZoneKey > 1000 && destinationParcel.ZoneKey < 1200))).ToFlag();// (destinationParcel.HouseholdsBuffer1 + destinationParcel.EmploymentTotalBuffer1 >= 31000).ToFlag();
 
+      int urbanFlag = ((tour.OriginParcel.District > 0 && tour.OriginParcel.District < 4) && (tour.OriginParcel.ZoneKey > 200 && !(tour.OriginParcel.ZoneKey > 1000 && tour.OriginParcel.ZoneKey < 1200))).ToFlag();
+      int cbdFlag = ((tour.OriginParcel.District > 0 && tour.OriginParcel.District < 4) && (tour.OriginParcel.ZoneKey > 50000)).ToFlag();
+
       if (mode == Global.Settings.Modes.ParkAndRide) {
         //alternative.AddUtilityTerm(211, tour.OriginParcel.CBD_AreaType_Buffer1());
         //alternative.AddUtilityTerm(212, tour.OriginParcel.Urban_AreaType_Buffer1());
@@ -37,8 +40,10 @@ namespace DaySim.ChoiceModels.Default.Models {
         // alternative.AddUtilityTerm(227, destinationParcel.PCA_MixedUseTerm_Buffer1());
         // alternative.AddUtilityTerm(228, destinationParcel.PCA_TransitAccessTerm_Buffer1());
       } else if (mode == Global.Settings.Modes.Transit) {
-        alternative.AddUtilityTerm(231, (tour.OriginParcel.District <= 2).ToFlag()); //tour.OriginParcel.CBD_AreaType_Buffer1());
-        alternative.AddUtilityTerm(232, (tour.OriginParcel.District == 3).ToFlag()); //tour.OriginParcel.Urban_AreaType_Buffer1());
+        // alternative.AddUtilityTerm(231, (tour.OriginParcel.District <= 2).ToFlag()); //tour.OriginParcel.CBD_AreaType_Buffer1());
+        alternative.AddUtilityTerm(231, cbdFlag);
+        // alternative.AddUtilityTerm(232, (tour.OriginParcel.District == 3).ToFlag()); //tour.OriginParcel.Urban_AreaType_Buffer1());
+        alternative.AddUtilityTerm(232, urbanFlag);
         alternative.AddUtilityTerm(233, (tour.OriginParcel.District == 4).ToFlag()); //tour.OriginParcel.Suburban_AreaType_Buffer1());
         alternative.AddUtilityTerm(234, (tour.OriginParcel.District >= 5).ToFlag()); //tour.OriginParcel.AllRural_AreaType_Buffer1());
         alternative.AddUtilityTerm(235, (destinationParcel.District <= 2).ToFlag()); //destinationParcel.CBD_AreaType_Buffer1());
